b360cded49b2f2a73b71599c3b02be05d3bc08d8
Homero Batista Kzam <homero.kzam@gmail.com>
2024-10-18 16:28:48 -0300

    first commit

---
+++ Python.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using CSnakes.Runtime;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder(args)
+  .ConfigureServices(services =>
+  {
+      var home = Path.Join(Environment.CurrentDirectory, "..", "Python.Core");
+      services
+          .WithPython()
+          .WithHome(home)
+          .FromConda("/opt/homebrew/Caskroom/miniconda/base")
+          .WithCondaEnvironment("projects_env");
+          // .FromEnvironmentVariable("Python3_ROOT_DIR", "3.12")
+          // .FromMacOSInstallerLocator("3.12")
+          // .WithPipInstaller();
+  });
+
+var app = builder.Build();
+
+var env = app.Services.GetRequiredService<IPythonEnvironment>();
+
+RunHelloWorld(env);
+
+static void RunHelloWorld(IPythonEnvironment env)
+{
+  var helloWorld = env.HelloWorld();
+}
---
+++ Python.Core/hello_world.py
@@ -0,0 +1,2 @@
+def format_name(name, max_length):
+	return "Hello {}".format(name.capitalize())[:max_length]
